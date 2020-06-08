0b96a5965af1cd56e04de0951f1db4d8bbd6f72f
Justice Eziefule <eziefulejustice@gmail.com>
2020-06-09 00:39:24 +0100

    fjhfjh

--- app.js
+++ app.js
@@ -82,4 +82,4 @@ app.use(covidRoutes);
 
 const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
